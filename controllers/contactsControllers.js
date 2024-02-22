cd3cb878b5c7e84da0bc0e0f012c00b940ada662
YaroslavKyba <narxoc.ne@gmail.com>
2024-02-22 23:29:06 +0200

    try

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -9,3 +9,9 @@ export const deleteContact = (req, res) => {};
 export const createContact = (req, res) => {};
 
 export const updateContact = (req, res) => {};
+
+// @ GET /api/contacts
+// @ GET /api/contacts/:id
+// @ DELETE /api/contacts/:id
+// @ POST /api/contacts
+// @ PUT /api/contacts/:id
